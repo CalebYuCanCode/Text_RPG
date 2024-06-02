c861cea18cffb6ebcaf8736829bc56b72fdcaffb
CalebYuCanCode <calebmyu99@gmail.com>
2024-06-02 18:04:44 -0400

    Add files via upload

--- Scientist.cpp
+++ Scientist.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+void Scientist_Class::set_stats(int new_health_stat, int new_damage_stat, int new_defense_stat, int new_agility_stat){//use this for leveling
+	// there must also be a way to make this a hero class function so you do not need to keep on making this for each class
+	health = new_health_stat;
+	damage = new_damage_stat;
+	defense = new_defense_stat;
+	agility = new_agility_stat;
+}
+
 void Scientist_Class::Scientist() {
 	health;
 	damage;
--- Scientist.h
+++ Scientist.h
@@ -9,6 +9,8 @@ class Scientist_Class : public hero
 
 public:
 
+	void set_stats(int new_health_stat, int new_damage_stat, int new_defense_stat, int new_agility_stat);
+
 	void Scientist();
 
 	void get_hero_stats();
