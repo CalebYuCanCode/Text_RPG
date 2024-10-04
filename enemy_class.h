04e6fdc629156df4c8146978aa72d9ca9ba5b81b
CalebYuCanCode <calebmyu99@gmail.com>
2024-10-04 07:56:53 -0400

    Update enemy_class.h

--- enemy_class.h
+++ enemy_class.h
@@ -7,4 +7,4 @@ string enemy_name;
 static int enemy_health;
 static int enemy_damage;
 static int enemy_defense;
-void enemy_sneck();
+void enemy(enemy_heallth, enemy_damage, enemy_defense, enemy_name);
